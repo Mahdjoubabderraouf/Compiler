a35b74f7727340ebf6b7180929e89509ce3c8aa1
Mahdjoubabderraouf <mahdjoub.abderraouf@gmail.com>
2023-12-03 00:24:08 +0100

    ts_function_add_in_liste

--- ts.c
+++ ts.c
@@ -36,6 +36,38 @@ typedef struct
 // element tab[1000];
 variable *tabVar;
 constant *tabConst;
+void addVariable(variable **tabVar, char *name, char *type, int state, float val, int globale)
+{
+    variable *newVar = (variable*)malloc(sizeof(variable));
+    newVar->state = state;
+    strcpy(newVar->name, name);
+    strcpy(newVar->type, type);
+    newVar->val = val;
+    newVar->globale = globale;
+    newVar->suiv = *tabVar;
+    *tabVar = newVar;
+}
+
+void addConstant(constant **tabConst, char *name, char *type, int state, float val)
+{
+    constant *newConst = (constant*)malloc(sizeof(constant));
+    newConst->state = state;
+    strcpy(newConst->name, name);
+    strcpy(newConst->type, type);
+    newConst->val = val;
+    newConst->suiv = *tabConst;
+    *tabConst = newConst;
+}
+
+void addElt(elt **tab, char *name, char *type, int state)
+{
+    elt *newElt = (elt*)malloc(sizeof(elt));
+    newElt->state = state;
+    strcpy(newElt->name, name);
+    strcpy(newElt->type, type);
+    newElt->suiv = *tab;
+    *tab = newElt;
+}
 // elt tabs[40],tabm[40];
 elt *tabs;
 elt *tabm;
